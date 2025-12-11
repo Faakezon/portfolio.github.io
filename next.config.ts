5cfdab7270035f16b165f1b35ca1f4584d5e63a2
Fabbe <faak92@gmail.com>
2025-12-11 16:15:29 +0100

    Updating next.config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
 
-const nextConfig: NextConfig = {
-  basePath: "/portfolio.github.io",
+const nextConfig = {
+  output: "export",
+  basePath: isProd ? "/portfolio.github.io" : "",
+  assetPrefix: isProd ? "/portfolio.github.io/" : "",
+  images: {
+    unoptimized: true
+  }
 };
 
 export default nextConfig;
