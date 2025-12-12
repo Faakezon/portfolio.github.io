da42deb226b92ffd475c0dcc99c5457410f0f5ac
Fabbe <faak92@gmail.com>
2025-12-12 13:26:21 +0100

    Removing trailing slash

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ export default function RootRedirect() {
     <>
       <Head>
         <title>Redirecting…</title>
-        <meta httpEquiv="refresh" content="0;url=/sv/" />
+        <meta httpEquiv="refresh" content="0;url=/sv" />
       </Head>
       <div className="flex items-center justify-center min-h-screen">
         Redirecting to Swedish version…
